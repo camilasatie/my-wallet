ee8b567483d14be3696e055f9e94d3f85413e6c7
Camila <camila.nagano@afya.com.br>
2021-02-18 07:43:52 -0300

    add global styles and theme

---
+++ src/styles/themes/light.ts
@@ -0,0 +1,16 @@
+export default {
+  title: 'light',
+  colors: {
+    primary: '#DCDCDC',
+    secondary: '#FFF',
+    tertiary: '#F5F5F5',
+
+    white: '#FFF',
+    black: '#000',
+    gray: '#BFBFBF',
+
+    success: '#03BB85',
+    info: '#3B5998',
+    warning: '#FF6961',
+  },
+};
